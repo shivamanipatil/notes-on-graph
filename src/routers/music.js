da3871a2ca5b4a831e6571ffbf685cc23f0c5cb9
Shivamani Patil <shivamanipatil10@gmail.com>
2020-05-13 13:14:55 +0530

    async/await refactor

--- src/routers/music.js
+++ src/routers/music.js
@@ -13,49 +13,48 @@ const DEFAULT_LIMIT = 3
 const URL = 'http://ws.audioscrobbler.com/2.0/'
 
 //get similar artists from queried artist
-router.get('/music/getSimilarArtists', auth, async (req, res) => {
-    if(!req.query.artist) {
-        res.send({error: "Please provide artist name."})
-    }    
-    payload['artist'] = req.query.artist
-    payload['method'] = 'artist.getSimilar'
-    payload['limit'] = req.query.limit? req.query.limit: DEFAULT_LIMIT
-    
-    //axios request to lastfm api
-    axios({
-        method: 'get',
-        url: URL,
-        params: payload,
-        headers: {'user-agent': 'q1ra'}
-    }).then((result) => {
-        res.send(result.data.similarartists.artist.map((artist) => artist.name))
-    }).catch((e) => {
-        res.status(400).send(e)  
-    })
+router.get('/music/getSimilarArtists', auth, async (req, res) => {    
+    try {
+        if(!req.query.artist) {
+            res.send({error: "Please provide artist name."})
+        }    
+        payload['artist'] = req.query.artist
+        payload['method'] = 'artist.getSimilar'
+        payload['limit'] = req.query.limit? req.query.limit: DEFAULT_LIMIT
+        const response = await axios({
+            method: 'get',
+            url: URL,
+            params: payload,
+            headers: {'user-agent': 'q1ra'}
+        })
+        res.send(response.data.similarartists.artist.map((artist) => artist.name))
+    } catch(e) {
+        res.status(400).send(e)
+    }
 })
 
 //get similar songs from queried artist and song
 router.get('/music/getSimilarSongs', auth, async (req, res) => {
-    if(!req.query.artist && !req.query.song) {
-        res.send({error: "Please provide artist and song."})
+    try {
+        if(!req.query.artist && !req.query.song) {
+            res.send({error: "Please provide artist and song."})
+        }
+        payload['artist'] = req.query.artist
+        payload['track'] = req.query.track
+        payload['limit'] = req.query.limit? req.query.limit: DEFAULT_LIMIT
+        payload['method'] = 'track.getSimilar'
+        
+        const response = await axios({
+            method: 'get',
+            url: URL,
+            params: payload,
+            headers: {'user-agent': 'q1ra'}
+        })
+        res.send(response.data.similartracks.track.map((track) => track.name))
+    } catch(e) {
+        res.status(400).send(e)
     }
-    payload['artist'] = req.query.artist
-    payload['track'] = req.query.track
-    payload['limit'] = req.query.limit? req.query.limit: DEFAULT_LIMIT
-    payload['method'] = 'track.getSimilar'
-    
-    axios({
-        method: 'get',
-        url: URL,
-        params: payload,
-        headers: {'user-agent': 'q1ra'}
-    }).then((result) => {
-        res.send(result.data.similartracks.track.map((track) => track.name))
-    }).catch((e) => {
-        res.status(400).send(e)  
-    })
 })
-
 
 
 
